8bfea43a0a21d1d39fff5f79c3c648469b09250b
Pavel Rabekin <pavelrabekin@gmail.com>
2024-07-18 16:24:50 +0300

    fix date

--- redux/redus-essentials-example-aaa/src/features/posts/PostList.js
+++ redux/redus-essentials-example-aaa/src/features/posts/PostList.js
@@ -6,12 +6,15 @@ import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+  const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <PostAuthor userId={post.user} />
       <p className="post-content">{post.content.substring(0, 100)}</p>
-      <TimeAgo timestamp={post.data} />
+      <TimeAgo timestamp={post.date} />
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
