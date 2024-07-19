2417ba0eec30d81019fe3ea7c67175af07e795e1
Pavel Rabekin <pavelrabekin@gmail.com>
2024-07-19 13:32:17 +0300

    add active nutton tracking

--- react-router/src/routes/root.jsx
+++ react-router/src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, NavLink } from "react-router-dom";
 
 export default function Root() {
   const { contacts } = useLoaderData();
@@ -9,13 +9,7 @@ export default function Root() {
         <h1>React Router Contacts</h1>
         <div>
           <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
+            <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
@@ -28,7 +22,10 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) => (isActive ? "active" : isPending ? "pending" : "")}
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -37,7 +34,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
