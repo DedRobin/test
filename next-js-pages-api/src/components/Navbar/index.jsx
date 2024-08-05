49e8164eefba8be191da7b7a09ea20d6e670b38b
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-05 16:25:48 +0300

    init: init commit

---
+++ next-js-pages-api/src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">First</a>
+        </li>
+        <li>
+          <a href="/">Second</a>
+        </li>
+        <li>
+          <a href="/">Third</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
