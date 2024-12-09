3b80e98af1bd173621427b46c5ad1ec8153a7f9a
Pavel Rabekin <pavelrabekin@gmail.com>
2024-12-09 12:25:04 +0300

    feat: add base

---
+++ js/index.js
@@ -0,0 +1,15 @@
+import { createServer } from 'node:http';
+
+const PORT = 3000;
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Listening on 127.0.0.1:${PORT}`);
+});
+
+// run with `node server.mjs`
